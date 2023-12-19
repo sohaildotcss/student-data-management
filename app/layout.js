2684988a2a217e8dcd47e412fc58875f4f0dc18f
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2023-12-19 20:33:04 +0530

    modified metadata title

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Salesforce",
+  title: "RUCE",
   description: "Cloud Based Student Data Management with Salesforce",
 };
 
