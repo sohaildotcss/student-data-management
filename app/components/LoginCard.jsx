c104337b1baafa3b3f8a3def7eddf4adebd74766
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2023-12-19 20:31:24 +0530

    updated authentication and made it ux-friendly

--- app/components/LoginCard.jsx
+++ app/components/LoginCard.jsx
@@ -1,31 +1,36 @@
-"use client";
-import { useState } from "react";
-import { FaLock } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
-import Link from "next/link";
-import "../globals.css";
+'use client';
+import { useState } from 'react';
+import { FaLock } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import Link from 'next/link';
+import '../globals.css';
 
 const checkAuth = () => {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  if (username === "1" && password === "1") {
-    window.open("https://heliums.netlify.app/dashboard");
-    // return <Link href="/dashboard"></Link>;
-    // return true;
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  if (username && !username === '') {
+    if (email === 'test' && password === 'test123') {
+      window.open('https://heliums.netlify.app/dashboard');
+      // return <Link href="/dashboard"></Link>;
+      // return true;
+    } else {
+      alert('Invalid username or password. Please try again.');
+    }
   } else {
-    alert("Invalid username or password. Please try again.");
+    alert('Please Enter your name.');
   }
 };
+// Dynamically changing name functionality
 function LoginCard() {
-  // Dynamically changing name functionality
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const handleChange = (event) => {
     setUsername(event.target.value);
   };
   return (
     <>
       <div className="right-section flex flex-1 items-center justify-center">
-        <div className="login-card z-10 flex h-auto w-auto flex-col gap-16 bg-slate-100 p-9 rounded-lg">
+        <div className="login-card z-10 flex h-auto w-auto flex-col gap-16 rounded-lg bg-slate-100 p-9">
           <div className="header flex justify-center">
             <img
               src="https://user-images.githubusercontent.com/39955420/147578264-bae0526c-028a-49d2-8af8-d08bb4edbd2a.gif"
@@ -33,15 +38,16 @@ function LoginCard() {
               className="mr-2 h-9 w-auto"
             />
             <h1 className="from-pink-500 to-violet-500 text-2xl font-extrabold text-black ease-in-out hover:bg-gradient-to-r hover:bg-clip-text hover:text-transparent hover:transition">
-              Welcome {username}{" "}
+              Welcome {username}{' '}
             </h1>
           </div>
           <div className="auth flex  flex-col gap-8">
-            <div className="input-forms w-96 flex flex-col gap-2">
+            <div className="input-forms flex w-96 flex-col gap-2">
               <label htmlFor="name" className="font-semibold">
                 Enter Your Name:
               </label>
               <input
+                id="username"
                 type="text"
                 onChange={handleChange}
                 required="true"
@@ -55,10 +61,10 @@ function LoginCard() {
                 <FaEnvelope className="faDuoTone ml-2 mt-0.5 text-[18px] text-pink-500" />
               </div>
               <input
-                id="username"
+                id="email"
                 className="block w-full  rounded-md border border-slate-300 py-2 pl-2 pr-3 shadow-sm placeholder:text-slate-400 placeholder:opacity-0 placeholder:transition-all placeholder:hover:opacity-100 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
                 type="text"
-                placeholder="name@ruce.ac.in"
+                placeholder="test"
               />
               <div className="flex">
                 <label htmlFor="password" className="font-semibold">
@@ -70,7 +76,7 @@ function LoginCard() {
                 id="password"
                 className=" block w-full  rounded-md border border-slate-300 py-2 pl-2 pr-3 shadow-sm placeholder:text-slate-400 placeholder:opacity-0 placeholder:transition-all placeholder:hover:opacity-100 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
                 type="password"
-                placeholder="•••••••"
+                placeholder="test123"
               />
             </div>
             <div className="login-btn flex flex-col bg-slate-100">
